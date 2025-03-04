fe912c5e31df2844dde4be3dcb42a0a2f87b0b6e
skorea6 <skorea6@naver.com>
2025-03-04 16:32:09 +0900

    Rest API 구축

--- main.go
+++ main.go
@@ -1,7 +1,50 @@
 package main
 
-import "mjcoin/explorer"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const (
+	port string = ":4000"
+)
+
+type URLDescriptions struct {
+	URL         string `json:"url"`
+	Method      string `json:"method"`
+	Description string `json:"description"`
+	Payload     string `json:"payload,omitempty"` // 데이터가 없으면 없애줌
+}
+
+func documentation(rw http.ResponseWriter, req *http.Request) {
+	data := []URLDescriptions{
+		{
+			URL:         "/",
+			Method:      "GET",
+			Description: "See Documentation",
+		},
+		{
+			URL:         "/blocks",
+			Method:      "POST",
+			Description: "Add A Block",
+			Payload:     "data:string",
+		},
+	}
+	rw.Header().Add("Content-Type", "application/json")
+	//b, err := json.Marshal(data)
+	//utils.HandleErr(err)
+	//fmt.Fprintf(rw, "%s", b)
+
+	json.NewEncoder(rw).Encode(data)
+}
 
 func main() {
-	explorer.Start()
+	http.HandleFunc("/", documentation)
+
+	fmt.Printf("Listening on port %s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
+
+	//explorer.Start()
 }
