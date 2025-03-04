cd3e4dc5cc9ed95bb2b303a14b7bc01b445e298a
skorea6 <skorea6@naver.com>
2025-03-04 15:46:01 +0900

    html template partial로 리펙토링

--- main.go
+++ main.go
@@ -8,7 +8,12 @@ import (
 	"net/http"
 )
 
-const port string = ":4000"
+const (
+	port        string = ":4000"
+	templateDir string = "templates/"
+)
+
+var templates *template.Template
 
 type homeData struct {
 	PageTitle string // 공유를 위해서는 대문자
@@ -16,26 +21,18 @@ type homeData struct {
 }
 
 func home(rw http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("templates/home.html"))
-	tmpl.Execute(rw, homeData{
+	templates.ExecuteTemplate(rw, "home", homeData{
 		PageTitle: "Home",
 		Blocks:    blockchain.GetBlockchain().AllBlocks(),
 	})
 }
 
 func main() {
+	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
+	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml"))
+
 	http.HandleFunc("/", home)
 
 	fmt.Printf("Listening on port %s\n", port)
 	log.Fatal(http.ListenAndServe(port, nil)) // exit 1로 종료하는 에러가 있으면 출력
-
-	//chain := blockchain.GetBlockchain()
-	//chain.AddBlock("Second Block")
-	//chain.AddBlock("Third Block")
-	//
-	//for _, block := range chain.AllBlocks() {
-	//	fmt.Printf("Data: %s\n", block.Data)
-	//	fmt.Printf("Hash: %s\n", block.Hash)
-	//	fmt.Printf("PrevHash: %s\n\n", block.PrevHash)
-	//}
 }
