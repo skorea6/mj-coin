ae3fb8e08a4e336602e18e588d25fa5ec9380392
skorea6 <skorea6@naver.com>
2025-03-04 15:06:46 +0900

    Http 서버 띄우기, home 템플릿 설정

--- main.go
+++ main.go
@@ -2,17 +2,39 @@ package main
 
 import (
 	"fmt"
-	"mjcoin/blockchain"
+	"html/template"
+	"log"
+	"net/http"
 )
 
-func main() {
-	chain := blockchain.GetBlockchain()
-	chain.AddBlock("Second Block")
-	chain.AddBlock("Third Block")
+const port string = ":4000"
 
-	for _, block := range chain.AllBlocks() {
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Hash: %s\n", block.Hash)
-		fmt.Printf("PrevHash: %s\n\n", block.PrevHash)
+type homeData struct {
+	PageTitle string
+}
+
+func home(rw http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("templates/home.html")
+	// go 에는 exception 이 없음 -> 따로 처리해줘야함
+	if err != nil {
+		log.Fatal(err)
 	}
+	tmpl.Execute(rw, homeData{PageTitle: "Home"})
+}
+
+func main() {
+	http.HandleFunc("/", home)
+
+	fmt.Printf("Listening on port %s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil)) // exit 1로 종료하는 에러가 있으면 출력
+
+	//chain := blockchain.GetBlockchain()
+	//chain.AddBlock("Second Block")
+	//chain.AddBlock("Third Block")
+	//
+	//for _, block := range chain.AllBlocks() {
+	//	fmt.Printf("Data: %s\n", block.Data)
+	//	fmt.Printf("Hash: %s\n", block.Hash)
+	//	fmt.Printf("PrevHash: %s\n\n", block.PrevHash)
+	//}
 }
