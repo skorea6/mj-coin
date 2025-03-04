445fdc897651448044c1ed4b11438707d969eae0
skorea6 <skorea6@naver.com>
2025-03-04 15:05:00 +0900

    기본적인 블록체인 구현 (+리팩토링)

---
+++ blockchain/blockchain.go
@@ -0,0 +1,69 @@
+package blockchain
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"sync"
+)
+
+type block struct {
+	Data     string
+	Hash     string
+	PrevHash string
+}
+
+type blockchain struct {
+	blocks []*block
+
+	/**
+	blocks []block과 같은 형태로 슬라이스를 선언하면,
+	슬라이스가 참조하는 배열이 복사되어 메모리 사용량이 늘어나게 됩니다.
+	이 때문에, 슬라이스가 매우 길어지는 경우에는 메모리 사용량이 매우 증가하게 되어 문제가 발생할 수 있습니다.
+
+	따라서, blocks []*block과 같이 포인터 슬라이스로 변경하면,
+	실제 block 객체가 아니라 객체의 '메모리 주소값'만 저장하게 되므로, 메모리 사용량이 줄어들게 됩니다.
+
+	이렇게 하면 인스턴스의 복사가 아니라
+	인스턴스의 메모리 주소값을 가져와서 메모리를 안정화시키며 최적화에 이점이 있습니다.
+	*/
+}
+
+var b *blockchain  // singleton 패턴
+var once sync.Once // 한번만 사용하기 위함
+
+func (b *block) calculateHash() {
+	hash := sha256.Sum256([]byte(b.Data + b.PrevHash))
+	b.Hash = fmt.Sprintf("%x", hash)
+}
+
+func getLatestHash() string {
+	totalBlocks := len(GetBlockchain().blocks)
+	if totalBlocks > 0 {
+		return GetBlockchain().blocks[totalBlocks-1].Hash
+	}
+	return ""
+}
+
+func createBlock(data string) *block { // 메모리 주소 반환!
+	newBlock := &block{data, "", getLatestHash()}
+	newBlock.calculateHash()
+	return newBlock
+}
+
+func (b *blockchain) AddBlock(data string) {
+	b.blocks = append(b.blocks, createBlock(data))
+}
+
+func GetBlockchain() *blockchain {
+	if b == nil {
+		once.Do(func() {
+			b = &blockchain{}
+			b.AddBlock("Genesis Block")
+		}) // 단 한번만 실행할수 있도록 (병렬의 상황에서도.)
+	}
+	return b
+}
+
+func (b *blockchain) AllBlocks() []*block {
+	return b.blocks
+}
--- main.go
+++ main.go
@@ -2,84 +2,17 @@ package main
 
 import (
 	"fmt"
+	"mjcoin/blockchain"
 )
 
-func plus(a int, b int) int {
-	return a + b
-}
-
-func plus2(a, b int, name string) (int, string) {
-	return a + b, name
-}
-
-func plus3(a ...int) int {
-	var total int
-	for _, item := range a {
-		total += item
-	}
-	return total
-}
-
-type person struct {
-	name string
-	age  int
-}
-
-func (p person) sayHello() {
-	fmt.Printf("Hello, %s!\n", p.name)
-}
-
 func main() {
-	fmt.Println("It Works!")
+	chain := blockchain.GetBlockchain()
+	chain.AddBlock("Second Block")
+	chain.AddBlock("Third Block")
 
-	//name2 := "이렇게도 쓸수 있다. 위와 같음 타입추론함."
-	age := 18
-	age = 12
-	fmt.Println(age)
-
-	result := plus(2, 2)
-	fmt.Println(result)
-
-	result3 := plus3(2, 3, 4, 5, 6, 7)
-	fmt.Println(result3)
-
-	var name string = "nico"
-	for index, letter := range name {
-		fmt.Println(index, letter)  // byte
-		fmt.Println(string(letter)) // string
-		fmt.Printf("%b\n", letter)  // 2진수
+	for _, block := range chain.AllBlocks() {
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %s\n", block.Hash)
+		fmt.Printf("PrevHash: %s\n\n", block.PrevHash)
 	}
-
-	x := 4848498445
-	fmt.Printf("%b\n", x)
-	xAsBinary := fmt.Sprintf("%b\n", x)
-	fmt.Println(x, xAsBinary)
-
-	// Array
-	foods := [3]string{"a", "b", "c"} // 개수 명시 필수
-	for _, food := range foods {
-		fmt.Println(food)
-	}
-	for i := 0; i < len(foods); i++ {
-		fmt.Println(foods[i])
-	}
-
-	// slice : 개수 length 무한히 커질수 있음
-	foods_slice := []string{"a", "b", "c"}
-	fmt.Printf("%v\n", foods_slice)
-	foods_slice = append(foods_slice, "d")
-	fmt.Printf("%v\n", foods_slice)
-	fmt.Println(len(foods_slice))
-
-	a := 2
-	b := &a // a의 메모리 '주소' 저장
-	a = 50
-	fmt.Println(b, &a)
-	fmt.Println(a)
-	fmt.Println(*b) // &는 주소, *는 값
-
-	nico := person{name: "Nico", age: 30}
-	nico2 := person{"Nico2", 31}
-	nico.sayHello()
-	fmt.Println(nico2.age)
 }
